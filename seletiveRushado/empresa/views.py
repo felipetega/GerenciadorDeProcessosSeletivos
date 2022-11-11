4f4235b26d122fe018d48f5fd0333db0ab9edd4d
Felipe Crema Ribeiro <100156699+felipetega@users.noreply.github.com>
2022-11-11 20:17:22 -0300

    32:28 - aula1

---
+++ seletiveRushado/empresa/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def nova_empresa(request):
+    return HttpResponse("Estou em nova_empresa")
