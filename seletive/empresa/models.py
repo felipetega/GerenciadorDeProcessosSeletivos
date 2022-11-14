caa058373530ee448b0561702fbd8c86ee824169
Felipe Crema Ribeiro <100156699+felipetega@users.noreply.github.com>
2022-11-13 22:56:15 -0300

    aula1 - 1:10

--- seletive/empresa/models.py
+++ seletive/empresa/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Tecnogias(models.Model):
+  tecnologia = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.tecnologia
+
+class Empresa(models.Model):
+  logo = models.ImageField(upload_to="logo_empresa", null=True)
+  nome = models.CharField(max_length=30)
+  email = models.EmailField()
+  cidade = models.CharField(max_length=30)
+  endereco = models.CharField(max_length=30)
+  caracteristica_empresa = models.TextField()
+  choices_nicho_mercado=(
+    ("M", "Marketing"),
+    ("N", "Nutrição"),
+    ("D", "Design")
+  )
+  nicho_mercado = models.CharField(max_length=3, choices=choices_nicho_mercado)
+  tecnologias = models.ManyToManyField(Tecnogias)
+
+  def __str__(self):
+    return self.nome
