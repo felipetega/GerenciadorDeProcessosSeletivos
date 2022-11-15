10129bc0bb29e4d15a6b6bd837e3e641e99cb53f
Felipe Crema Ribeiro <100156699+felipetega@users.noreply.github.com>
2022-11-15 19:06:27 -0300

    aula 1 - 1:58:50

--- seletive/empresa/views.py
+++ seletive/empresa/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Tecnologias
+from .models import Tecnologias, Empresa
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.contrib.messages import constants
 
 
 def nova_empresa(request):
@@ -16,4 +19,29 @@ def nova_empresa(request):
         caracteristicas = request.POST.get('caracteristicas')
         tecnologias = request.POST.getlist('tecnologias')
         logo = request.FILES.get('logo')
-        return HttpResponse(tecnologias)
+
+        if (len(nome.strip()) == 0 or len(email.strip()) == 0 or len(cidade.strip()) == 0 or len(endereco.strip()) == 0 or len(nicho.strip()) == 0 or len(caracteristicas.strip()) == 0 or (not logo)): 
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+            return redirect('/home/nova_empresa')
+
+        if logo.size > 100_000_000:
+            messages.add_message(request, constants.ERROR, 'A logo da empresa deve ter menos de 10MB')
+            return redirect('/home/nova_empresa')
+
+        if nicho not in [i[0] for i in Empresa.choices_nicho_mercado]:
+            messages.add_message(request, constants.ERROR, 'Nicho de mercado inválido')
+            return redirect('/home/nova_empresa')
+        
+        empresa = Empresa(logo=logo,
+                        nome=nome,
+                        email=email,
+                        cidade=cidade,
+                        endereco=endereco,
+                        nicho_mercado=nicho,
+                        caracteristica_empresa=caracteristicas)
+
+        empresa.save()
+        empresa.tecnologias.add(*tecnologias)
+        empresa.save()
+        messages.add_message(request, constants.SUCCESS, 'Empresa cadastrada com sucesso')
+        return redirect('/home/nova_empresa')
