4185fd1bb949bcf50eb1ea7c7f4c703304b7819c
Felipe Crema Ribeiro <100156699+felipetega@users.noreply.github.com>
2022-11-15 14:25:39 -0300

    aula1 - 1:39

--- seletive/empresa/views.py
+++ seletive/empresa/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Tecnologias
 
 
 def nova_empresa(request):
-    return render(request, 'nova_empresa.html')
+    if request.method == "GET": 
+        techs = Tecnologias.objects.all()
+        return render(request, 'nova_empresa.html', {'techs':techs})
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')        
+        cidade = request.POST.get('cidade')
+        endereco = request.POST.get('endereco')
+        nicho = request.POST.get('nicho')
+        caracteristicas = request.POST.get('caracteristicas')
+        tecnologias = request.POST.getlist('tecnologias')
+        logo = request.FILES.get('logo')
+        return HttpResponse(tecnologias)
