1a48d70737611e9fa8789770097c36c07cad3135
Felipe Crema Ribeiro <100156699+felipetega@users.noreply.github.com>
2022-11-16 14:31:53 -0300

    aula 2 - 14:00

--- seletive/empresa/views.py
+++ seletive/empresa/views.py
@@ -48,8 +48,9 @@ def nova_empresa(request):
 
 def empresas(request):
     empresas = Empresa.objects.all()
-    tecnologias = Tecnologias.objects.all()
-    return render(request, 'empresa.html', {'empresas': empresas,'tecnologias': tecnologias})
+    #empresa_teste = Empresa.objects.get(nome='Discord')
+    #print(empresa_teste.qtd_vagas())
+    return render(request, 'empresa.html', {'empresas': empresas})
 
 def excluir_empresa(request, id):
     empresa = Empresa.objects.get(id=id)
